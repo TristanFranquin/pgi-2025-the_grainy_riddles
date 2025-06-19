cc8c67f7f63881641763f1ed66f84f7b63ebea28
TristanFranquin <190858203+TristanFranquin@users.noreply.github.com>
2025-06-19 20:12:08 +0200

    déclaration fonctions  features.h
    max_componenent min_component stat_report

--- src/features.h
+++ src/features.h
@@ -1,10 +1,13 @@
 #ifndef FEATURES_H
 #define FEATURES_H
 
-
 void helloWorld();
 void mirror_horizontal(char *source_path);
 void rotate_acw(char *source_path);
 void mirror_vertical(char *source_path);
 void scale_crop(char *sourcepath, int center_X, int center_Y, int W, int H);
 #endif
+
+char* max_component(char *source_path, char color_pixel);
+char* min_component(char *source_path, char color_pixel);
+void stat_report(char *source_path);
