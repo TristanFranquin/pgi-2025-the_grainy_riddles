30d37a3fb707eb6d712af910812e2cc6ae8acc70
TristanFranquin <190858203+TristanFranquin@users.noreply.github.com>
2025-06-19 20:13:17 +0200

    déclaration fonctions utils.h
    color_red color_green color_blue

--- src/utils.h
+++ src/utils.h
@@ -7,3 +7,6 @@
  * - void setPixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y, pixelRGB* pixel);
  *
 */
+void color_red(char *source_path);
+void color_green(char *source_path);
+void color_blue(char *source_path);
