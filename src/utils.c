b3c38c56630cc666b7b143fffda0b1bdba3810ee
TristanFranquin <190858203+TristanFranquin@users.noreply.github.com>
2025-06-19 20:30:59 +0200

    color_red utils.c

--- src/utils.c
+++ src/utils.c
@@ -8,6 +8,18 @@
  * 
  */
 
-
+void color_red(char *source_path){
+    unsigned char* data=NULL;
+    int W,H,CH,X,Y;
+    read_image_data(source_path,&data,&W,&H,&CH);
+    for(Y=0;Y<H;Y++){
+        for(X=0;X<W;X++){
+            pixelRGB*pixel=get_pixel(data,W,H,CH,X,Y);
+            pixel->G=0;
+            pixel->B=0;
+        }
+    }
+    write_image_data("image_out.bmp",data,W,H);
+}
 
 
